200798bf3acdc8b4859f6861be2f608a93a67d02
NatC02 <natanceballos02@gmail.com>
2025-03-18 21:15:34 -0400

    startup files

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Canvas } from '@react-three/fiber';
+import { Leva } from 'leva';
+import { useState } from 'react';
+import Scene from './components/Scene/Scene';
+import * as THREE from 'three';
+import Contents from './components/Contents/Contents';
+
+function App() {
+	return (
+		<>
+			<Leva />
+			<div className='background-canvas'>
+				<Canvas
+					shadows
+					gl={{
+						antialias: false,
+						toneMapping: THREE.ACESFilmicToneMapping,
+					}}>
+					<Scene />
+				</Canvas>
+			</div>
+			<Contents />
+		</>
+	);
+}
+
+export default App;
